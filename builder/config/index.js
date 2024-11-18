69a6bc71955e3120f068e38d5549141896474c1b
fiftyseven <165079833+fiftys7vencode@users.noreply.github.com>
2024-11-18 17:45:41 +0100

    Update index.js

--- builder/config/index.js
+++ builder/config/index.js
@@ -1,22 +1,21 @@
 const usrname = document.getElementById("usernameinput");
 const agree = document.getElementById("agreetotos");
 
-
 if (localStorage.getItem("setupCompleted")) {
     location.assign("/index.html");
 };
 
 function completeSetup() {
-    if(usrname.value) {
-        if(agree.checked) {
-            localStorage.username = usrname.value;
-            localStorage.setupCompleted = true;
-            localStorage.sites = JSON.stringify([]);
+    if (usrname.value) {
+        if (agree.checked) {
+            localStorage.setItem("username", usrname.value);
+            localStorage.setItem("setupCompleted", true);
+            localStorage.setItem("sites", JSON.stringify([]));
             location.assign("/builder/index.html");
         } else {
-            alert("Error: You are not elegible for our service beacuse you don't agree to the TOS.")
+            alert("Error: You are not eligible for our service because you don't agree to the TOS.");
         }
     } else {
         alert("Error: Please fill the username field.");
-    };
+    }
 };
